505f4ab9cb83262ae2d17779081b5547259afd30
Benjamin Lee <ben@2nproductions.com>
2016-03-03 21:13:00 -0800

    Initial Commit

---
+++ BenGameEngine/BenGameEngine.h
@@ -0,0 +1,13 @@
+//
+//  BenGameEngine.h
+//  BenGameEngine
+//
+//  Created by Benjamin Lee on 3/3/16.
+//  Copyright © 2016 Benjamin Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BenGameEngine : NSObject
+
+@end
---
+++ BenGameEngine/BenGameEngine.m
@@ -0,0 +1,13 @@
+//
+//  BenGameEngine.m
+//  BenGameEngine
+//
+//  Created by Benjamin Lee on 3/3/16.
+//  Copyright © 2016 Benjamin Lee. All rights reserved.
+//
+
+#import "BenGameEngine.h"
+
+@implementation BenGameEngine
+
+@end
