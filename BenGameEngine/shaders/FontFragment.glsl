8defcaa43d073dfd000d0e68465d1dc06913d497
Benjamin Lee <ben@2nproductions.com>
2016-10-04 15:49:32 -0700

    Simplify shader

--- BenGameEngine/shaders/FontFragment.glsl
+++ BenGameEngine/shaders/FontFragment.glsl
@@ -3,10 +3,7 @@ varying lowp vec2 TexCoordOut;
 uniform sampler2D Texture;
 
 void main(void) {
+//    gl_FragColor = texture2D(Texture, TexCoordOut).aaaa ;
     gl_FragColor = texture2D(Texture, TexCoordOut).aaaa * SourceColor;
-    //gl_FragColor = frag;
-//    if (gl_FragColor.a < 0.9 && gl_FragColor.a != 0.0) {
-//        gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-//    }
 }
 
